44926e775e138f091e857be6ee1d6fc7fc6613f6
Pranav Sitaram Kapadnis <pranavkapadnis007@gmail.com>
2025-10-23 18:17:46 -0500

    My first Program, with addition of comments.

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -1,7 +1,16 @@
 package ex_01_Java_Basic;
 
+// This is a Class
 public class Lab001_HelloWorld {
+
+    // It is mandatory to add main function (Method)
     public static void main(String[] args) {
+
+        // Statement
         System.out.println("Hello World, I have written my first Program.");
     }
 }
+
+
+
+// Note: You cannot add two same signatured main functions in one class.
