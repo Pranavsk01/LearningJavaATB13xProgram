07fe54a0bd824550d3ef15499a22fc8251fce16a
Pranav Sitaram Kapadnis <pranavkapadnis007@gmail.com>
2025-10-23 19:14:31 -0500

    Keyword and Identifiers

---
+++ src/ex_01_Java_Basic/Lab003_Keywords_Identifiers.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("How many keyword and Identifiers do you see here?");
+    }
+}
+
+/*
+Keywords: 5
+Identifiers: 3 (args is also identifiers)
+ */
